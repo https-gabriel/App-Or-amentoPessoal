5ac50d08f31220bb0e70bb0a11bbaf885ab2649c
Gabriel Farias <farias2752@gmail.com>
2024-01-22 10:27:38 -0300

    Cadastro de despesas

--- app-orcamentoPessoal/app.js
+++ app-orcamentoPessoal/app.js
@@ -1,14 +1,3 @@
-class Despesas {
-    constructor (ano, mes, dia, tipo, descricao, valor) {
-        this.ano = ano
-        this.mes = mes
-        this.dia = dia 
-        this.tipo = tipo
-        this.descricao = descricao
-        this.valor = valor
-    }
-}
-
 function cadastrarDespesas() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
@@ -17,15 +6,4 @@ function cadastrarDespesas() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-    let despesas = new Despesas(
-        ano.value,
-        mes.value, 
-        dia.value, 
-        tipo.value, 
-        descricao.value, 
-        tipo.value, 
-        valor.value
-    )
-
-    console.log(despesas)
 }
